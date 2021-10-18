f09a42718299154d6a4c5e77dab38020950f42c5
panchao5 <heydumboz@gmail.com>
2021-10-18 23:32:58 +0800

    chore: remove logs

--- gatsby-theme-lime/src/utils/screens.ts
+++ gatsby-theme-lime/src/utils/screens.ts
@@ -1,7 +1,6 @@
 import { css, theme, TwStyle } from "twin.macro";
 
 export const maxXL = (content: string | TwStyle) => {
-  console.log(theme`screens.xl`);
   return css`
     @media (max-width: ${theme`screens.xl`}) {
       ${content}
